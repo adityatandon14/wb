443cc8198d10451997bfc03391a0ed30832e1ee0
adityatandon14 <you@example.com>
2020-04-04 15:50:47 +0530

    Updates

---
+++ precache-manifest.677ef7c737ac7ff8b3ef5825ccc21ce6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "755c4494fa665dab35ad6a909dd00743",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8920e01b74950b7eea67",
+    "url": "/static/css/2.5458e0fc.chunk.css"
+  },
+  {
+    "revision": "092f3b4be994e7a857a0",
+    "url": "/static/css/main.4b67b785.chunk.css"
+  },
+  {
+    "revision": "8920e01b74950b7eea67",
+    "url": "/static/js/2.976cbeb8.chunk.js"
+  },
+  {
+    "revision": "db4833b02f62bd470bb1579a2ec8a284",
+    "url": "/static/js/2.976cbeb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "092f3b4be994e7a857a0",
+    "url": "/static/js/main.04d80e00.chunk.js"
+  },
+  {
+    "revision": "700cce3a7caf4f992d6c",
+    "url": "/static/js/runtime-main.ebe91ffd.js"
+  }
+]);
